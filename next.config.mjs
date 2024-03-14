37a39834a128945d49a1fa6e12cca6174dd67458
Armando Hernández Rivera <Armando.hernandez.r@hotmail.com>
2024-03-13 19:17:05 -0600

    add the new domain

--- next.config.mjs
+++ next.config.mjs
@@ -5,20 +5,20 @@ const nextConfig = {
       {
         // Habilitar CORS para todas las solicitudes a la API
         source: "/api/urls", // Puedes ajustar la ruta según tu configuración
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Permitir solicitudes desde cualquier origen
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,DELETE,PUT", // Métodos HTTP permitidos
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Authorization, jwt", // Encabezados permitidos
-          },
-        ],
+        "headers": [
+        {
+          "key": "Access-Control-Allow-Origin",
+          "value": "*"
+        },
+        {
+          "key": "Access-Control-Allow-Methods",
+          "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+        },
+        {
+          "key": "Access-Control-Allow-Headers",
+          "value": "Content-Type, Authorization, jwt"
+        }
+      ],
       },
     ];
   },
